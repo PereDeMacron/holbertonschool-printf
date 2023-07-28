fd8cd49a9716f200941d5d373fc0f07fecce3730
RaphSchp <raphaelschopfer@gmail.com>
2023-07-28 17:30:34 +0000

    fix betty errors

--- decimal.c
+++ decimal.c
@@ -16,49 +16,49 @@
 
 int decimal(const char *format, ...)
 {
-    int printed_chars = 0;
-    va_list args;
-    const char *str;
-    char *p;
+	int printed_chars = 0;
+	va_list args;
+	const char *str;
+	char *p;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (str = format; str && *str != '\0'; str++)
-    {
-        if (*str == '%')
-        {
-            if (*(str + 1) == '%')
-            {
-                _putchar('%');
-                printed_chars++;
-                str++;
-            }
-            else if (*(str + 1) == 'd')
-            {
-                int num = va_arg(args, int);
-                char buffer[20];
-                snprintf(buffer, sizeof(buffer), "%d", num);
-                for (p = buffer; *p != '\0'; p++)
-                {
-                    _putchar(*p);
-                    printed_chars++;
-                }
-                str++;
-            }
-            else
-            {
-                _putchar(*str);
-                printed_chars++;
-            }
-        }
-        else
-        {
-            _putchar(*str);
-            printed_chars++;
-        }
-    }
+	for (str = format; str && *str != '\0'; str++)
+	{
+		if (*str == '%')
+		{
+			if (*(str + 1) == '%')
+			{
+				_putchar('%');
+				printed_chars++;
+				str++;
+			}
+			else if (*(str + 1) == 'd')
+			{
+				int num = va_arg(args, int);
+				char buffer[20];
+				snprintf(buffer, sizeof(buffer), "%d", num);
+				for (p = buffer; *p != '\0'; p++)
+				{
+					_putchar(*p);
+					printed_chars++;
+				}
+				str++;
+			}
+			else
+			{
+				_putchar(*str);
+				printed_chars++;
+			}
+		}
+		else
+		{
+			_putchar(*str);
+			printed_chars++;
+		}
+	}
 
-    va_end(args);
+	va_end(args);
 
-    return (printed_chars);
+	return (printed_chars);
 }
