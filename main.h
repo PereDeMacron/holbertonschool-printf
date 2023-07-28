df1a6caaca33ff3fe5c66ad838470849c622d3e4
PereDeMacron <peredemacron@gmail.com>
2023-07-28 03:22:59 -0700

    add a functional _printf with function 'C, S, I, D, %'

--- main.h
+++ main.h
@@ -4,7 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 
+int _putchar(char c);
+int c (const char *format, ...);
+int d (const char *format, ...);
+int i (const char *format, ...);
+int s (const char *format, ...);
+int percent(const char *format, ...);
 int _printf(const char *format, ...);
 
 #endif
